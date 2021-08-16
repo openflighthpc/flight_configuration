f4bf7a123fdbb070264fb219bd74ae79fd447833
Ben Armston <ben.armston@alces-software.com>
2021-08-16 17:27:23 +0100

    Bump to 0.5.0

--- lib/flight_configuration/version.rb
+++ lib/flight_configuration/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/openflighthpc/flight_configuration
 #==============================================================================
 module FlightConfiguration
-  VERSION = "0.4.1"
+  VERSION = "0.5.0"
 end
